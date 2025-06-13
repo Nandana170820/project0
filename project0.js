b5f73894d9ffcdb0a7f5d202fa4d4ce1c12a5584
Nandana170820 <120004413+Nandana170820@users.noreply.github.com>
2025-06-13 14:54:28 +0530

    Add files via upload

---
+++ project0.js
@@ -0,0 +1,8 @@
+function contactMe() {
+  window.scrollTo({ top: document.body.scrollHeight, behavior: 'smooth' });
+}
+
+function downloadResume() {
+  alert("Resume download not set up. Add your file link in the script.");
+  // You can replace alert with window.location.href = 'your-resume-link.pdf';
+}
